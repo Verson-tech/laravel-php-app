cc2c14363d24bbd0109d18227d141cbcb904ce31
Vira Murray <vira.avramenko@gmail.com>
2024-04-01 17:21:22 -0400

    capturing the user details

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user', function () {
-    // return "User is being loaded...";
-    //once the route is created in the view folder named user.blade.php, the route will be loaded:
-    return view('user');
+
+//capturing the user details:
+Route::get('/user/{id}', function($id){
+    return "User $id";
 });

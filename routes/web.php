bfb3e29ffdb1e78de57cfa1475687621021b09a6
Vira Murray <vira.avramenko@gmail.com>
2024-04-01 17:10:17 -0400

    added test route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    // return "User is being loaded...";
+    //once the route is created in the view folder named user.blade.php, the route will be loaded:
+    return view('user');
+});
